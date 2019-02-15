b3755e543451b2f07d80c466fb7cf265c8caf487
Carson Farmer <carson.farmer@gmail.com>
2019-02-15 01:00:32 -0700

    feature(url): adds url parse and a test

--- src/Containers/Scanner/index.tsx
+++ src/Containers/Scanner/index.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
+// import urlParse from 'url-parse'
+import { Alert } from 'react-native'
 import { View, Text, TouchableOpacity } from 'react-native'
 import MainActions, { MainState } from '../../Redux/MainRedux'
 import QRCodeScanner from 'react-native-qrcode-scanner'
@@ -21,24 +23,34 @@ type Props = StateProps & DispatchProps
 class Scanner extends Component<Props> {
   state = {  }
 
-  onSuccess(e: any) {
-    console.log('An error occured', e.data)
+  onSuccess = (e: any) => {
+    console.log('Scanned', e.data)
+    // const parsed = urlParse.parse(e.data)
+    Alert.alert(
+      JSON.stringify(e.data),
+      'Alert Msg',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel'
+        },
+        { text: 'OK', onPress: () => NavigationService.navigate('Home') }
+      ],
+      { cancelable: false }
+    )
   }
-
 
   render() {
     return (
       <View style={styles.container}>
         <QRCodeScanner
-          onRead={this.onSuccess.bind(this)}
-          topContent={
-            <Text>
-              Go to wikipedia.org/wiki/QR_code on your computer and scan the QR code.
-          </Text>
-          }
+          onRead={this.onSuccess}
+          topContent={<Text>Scan barcode</Text>}
           cameraProps={{ captureAudio: false }}
           bottomContent={
             <TouchableOpacity
+              // tslint:disable-next-line:jsx-no-lambda
               onPress={() => { NavigationService.navigate('Home') }}
             >
               <Text>Close</Text>
